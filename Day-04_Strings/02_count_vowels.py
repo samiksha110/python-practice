1f56f63da3b0a8c3100747e56607d316dcd9f368
samiksha110 <samikshalonkar1106@gmail.com>
2026-07-09 19:02:50 +0530

    count number of vowels

---
+++ Day-04_Strings/02_count_vowels.py
@@ -0,0 +1,11 @@
+# count the number of vowels
+
+text = input("Enter a string: ").lower()
+
+count = 0
+
+for char in text:
+    if char in "aeiou":
+        count += 1
+
+print("Number of vowels:", count)
