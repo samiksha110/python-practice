4052fff1ab30d499323b40097a9e3d9715ae7815
samiksha110 <samikshalonkar1106@gmail.com>
2026-07-11 19:13:51 +0530

    count frequency of character

---
+++ Day-04_Strings/08_count_frequency_of_character.py
@@ -0,0 +1,18 @@
+#  Count Frequency of a Character
+
+text = input("Enter a string: ")
+char = input("Enter character: ")
+
+print(text.count(char))
+
+# text = input("Enter a string: ")
+# char = input("Enter character: ")
+
+# count = 0
+
+# for ch in text:
+
+# if ch == char:
+# count += 1
+
+# print(count)
