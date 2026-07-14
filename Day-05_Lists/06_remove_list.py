2db09fc7bcffb19a44564d7b4f18f45fcc289aa8
samiksha110 <samikshalonkar1106@gmail.com>
2026-07-14 08:57:54 +0530

    remove in list

---
+++ Day-05_Lists/06_remove_list.py
@@ -0,0 +1,7 @@
+# 6] Remove "orange" from the list by value using remove().
+
+fruits = ['apple', 'orange', 'pineapple',
+          'mango', 'strawberry', 'guava', 'kiwi']
+fruits.remove("orange")
+
+print(fruits)
