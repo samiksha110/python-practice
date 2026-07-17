d65d1fbdaeac5202b70910c0ec8f307705ec3bab
samiksha110 <samikshalonkar1106@gmail.com>
2026-07-17 21:52:23 +0530

    function with return

---
+++ Day-07_Functions/04_funct_with_return.py
@@ -0,0 +1,12 @@
+#  Problem 4: First Function with return
+# Write a function named square that:
+# Takes one number as a parameter.
+# Returns its square.
+
+def square(n):
+    return (n * n)
+
+
+x = square(4)
+
+print(x)
