df47699e0db338be5f0518c6d2cc9cb6e6e5bbf3
samiksha110 <samikshalonkar1106@gmail.com>
2026-07-18 17:53:36 +0530

    count even no in list

--- Day-07_Functions/10_count_even_no_list.py
+++ Day-07_Functions/10_count_even_no_list.py
@@ -1,1 +1,17 @@
 # It should return how many even numbers are in the list.
+
+def count_even(numbers):
+    count = 0
+
+    for num in numbers:
+        if num % 2 == 0:
+            count += 1
+
+    return count
+
+
+numbers = [12, 45, 8, 90, 32]
+
+result = count_even(numbers)
+
+print(result)
