14b03dfa9bcbeb49ef144852234aee6038001dc0
samiksha110 <samikshalonkar1106@gmail.com>
2026-07-18 17:22:03 +0530

    print the largest number

---
+++ Day-07_Functions/06_largest_number.py
@@ -0,0 +1,15 @@
+# Write a function:
+
+# largest(a, b)
+
+# It should return the larger number.
+
+def largest(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+print(largest(10, 25))
+print(largest(50, 20))
