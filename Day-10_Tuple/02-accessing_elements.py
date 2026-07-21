ad2439878867f88794f6a0729178fc9ae65af99b
samiksha110 <samikshalonkar1106@gmail.com>
2026-07-21 22:15:31 +0530

    acess a element in tuple

---
+++ Day-10_Tuple/02-accessing_elements.py
@@ -0,0 +1,13 @@
+# Access Elements
+
+# Write the code to print: First element, Last element, Third element
+
+# Given: numbers = (10, 20, 30, 40, 50)
+
+numbers = (10, 20, 30, 40, 50)
+
+print(numbers[0])
+
+print(numbers[-1])
+
+print(numbers[2])
